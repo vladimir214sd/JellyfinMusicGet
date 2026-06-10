12c6e1900b05362fad8a281dcd002b71e05d0775
vladimir214sd <159032035+vladimir214sd@users.noreply.github.com>
2026-06-10 20:53:49 +0500

    Remove src/Jellyfin.Plugin.AuddMusicRecognition/Services/AudioFingerprint.cs

--- src/Jellyfin.Plugin.AuddMusicRecognition/Services/AudioFingerprint.cs
+++
@@ -1,8 +0,0 @@
-namespace Jellyfin.Plugin.AuddMusicRecognition.Services;
-
-/// <summary>
-/// Audio fingerprint data produced by Chromaprint.
-/// </summary>
-/// <param name="Fingerprint">Chromaprint fingerprint.</param>
-/// <param name="DurationSeconds">Fingerprint duration in seconds.</param>
-public sealed record AudioFingerprint(string Fingerprint, double DurationSeconds);
